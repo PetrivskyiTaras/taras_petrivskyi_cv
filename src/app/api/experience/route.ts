7050a94c81980f84d0b2ce3026655b2fedf70ac1
PetrivskyiTaras <34395038+PetrivskyiTaras@users.noreply.github.com>
2025-08-07 16:22:32 +0300

    Update skills and add new CV file, added Redash (#1)

--- src/app/api/experience/route.ts
+++ src/app/api/experience/route.ts
@@ -6,8 +6,8 @@ export const GET = () => {
       companyName: 'Radency',
       startDate: '2023-01-31T22:00:00.000Z',
       projectName: 'Project from scratch (USA)',
-      responsibilities: 'project from scratch, architecture planning (front end), all technical solutions, creating tasks, development of new features, writing unit tests, code review, mentoring',
-      technologies: 'TypeScript, React, Next.js, React Hook Form, JSX, Jest + React Testing Library, Material-UI, GraphQL/Apollo, TanStack Query (FKA React Query), Chart.js',
+      responsibilities: 'project from scratch, architecture planning (front end), all technical solutions, creating tasks, development of new features, writing unit tests, code review, mentoring, small BackEnd tasks using Kotlin and Express',
+      technologies: 'TypeScript, React, Next.js, React Hook Form, JSX, Jest + React Testing Library, Material-UI, GraphQL/Apollo, TanStack Query (FKA React Query), Chart.js, Redash',
       tools: 'GitLab, WebStorm, GIT, Jira, Figma, Miro',
     },
     {
